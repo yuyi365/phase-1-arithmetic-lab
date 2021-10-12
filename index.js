b35302075edf52eb28d7433a525b45b360688d85
Yuyi <yuyi365@gmail.com>
2021-10-12 19:22:11 -0400

    passed

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 31;
+let num2 = 2;
+let num3 = 10;
+let num4 = 6;
 
-// Write your code here
+const multiply = num1 * num2;
+
+const random = Math.ceil(Math.random() * 10);
+
+const mod = num3 % num4;
+
+const max = Math.max(5,6, 10, 2, 20);
